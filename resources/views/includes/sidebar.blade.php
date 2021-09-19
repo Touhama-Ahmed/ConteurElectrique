e263e0b391ec864fa594689e8ee417d73b3063e9
touhama-ahmed <totouhama@gmail.com>
2021-09-19 18:59:46 +0100

    design

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SMARTYLAB</div>
+        <div class="sidebar-brand-text mx-3">SMARTILAB</div>
     </a>
 
     <!-- Divider -->
