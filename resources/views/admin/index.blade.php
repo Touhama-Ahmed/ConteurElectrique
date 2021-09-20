2e1b92666795eceacf75d9a7930d474453f44f15
touhama-ahmed <totouhama@gmail.com>
2021-09-20 14:16:54 +0100

    design

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -48,7 +48,7 @@
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalU}}</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
