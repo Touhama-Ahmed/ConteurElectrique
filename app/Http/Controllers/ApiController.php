57bfbf9319a78f1272889d4ced66d35ca24eef61
touhama-ahmed <totouhama@gmail.com>
2021-09-28 12:30:09 +0100

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -42,7 +42,7 @@ class ApiController extends Controller
     {
 //        http://127.0.0.1:8000/addConsomation?Id_Maison=123&Isactive=1&courrant=14&tension=2&energie=45&Fpuissance=3&frequence=47&puissance=1
 
-        $data = $request->getContent();
+        $data = $request->getContent()->data;
 
         return response()->json([
             '1' => $data,
