0b9bfad08dd70bdd66d4d01ccb8011a80bbb412e
touhama-ahmed <totouhama@gmail.com>
2021-09-28 12:40:40 +0100

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -42,11 +42,10 @@ class ApiController extends Controller
     {
 //        http://127.0.0.1:8000/addConsomation?Id_Maison=123&Isactive=1&courrant=14&tension=2&energie=45&Fpuissance=3&frequence=47&puissance=1
 
-        $data = $request->getContent();
+        $data = $request->input('data');
 
         return response()->json([
             '1' => $data,
-
         ]);
         // Sending data to our repository
         $success = AdminRepository::createNewConsomation($data);
