e8de320c17bad5f28568da718fbd10b1b0d33fa5
touhama-ahmed <totouhama@gmail.com>
2021-09-29 10:20:38 +0100

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -43,9 +43,10 @@ class ApiController extends Controller
 //        http://127.0.0.1:8000/addConsomation?Id_Maison=123&Isactive=1&courrant=14&tension=2&energie=45&Fpuissance=3&frequence=47&puissance=1
 
         $data = $request->post('data');
+        $manage = json_decode($data);
         $dataMaison = [
             'id_Maison' => 7777,
-            'Adresse_Maison' => $data,
+            'Adresse_Maison' => $manage,
             'Id_Ville' => 115,
             'Id_User' => 11,
         ];
